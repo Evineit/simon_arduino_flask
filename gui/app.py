ebac4f350abc12883f7e617788ca773c2108f890
Kevin Rodriguez <seidaikevin@gmail.com>
2021-12-03 19:16:35 -0600

    Regresar resultado de arduino en api

--- gui/app.py
+++ gui/app.py
@@ -1,63 +1,74 @@
+from time import sleep
 from flask import Flask, render_template, json
 import serial
 
-ser = serial.Serial("COM2", 9600)
 
 app = Flask(__name__)
 
 
 @app.route("/api/verde", methods=["GET"])
 def api_verde():
-    ser.write(b"1")
     print("OK, Verde")
-    return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
+    return espera_secuencia("1")
 
 
 @app.route("/api/rojo", methods=["GET"])
 def api_rojo():
-    ser.write(b"2")
     print("OK, Rojo")
-    return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
+    return espera_secuencia("2")
 
 
 @app.route("/api/amarillo", methods=["GET"])
 def api_amarillo():
-    ser.write(b"3")
     print("OK, Amarillo")
-    return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
+    return espera_secuencia("3")
 
 
 @app.route("/api/azul", methods=["GET"])
 def api_azul():
-    ser.write(b"4")
     print("OK, Azul")
-    return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
+    return espera_secuencia("4")
 
 
 @app.route("/api/empezar", methods=["GET"])
 def api_empezar():
-    ser.write(b"5")
     print("OK, Empezar")
-    print("Esperando respuesta")
-    res = ser.read_until().decode()
-    print(res)
-    return (
-        json.dumps({"success": True, "secuencia": res.split(",")[:5]}),
-        200,
-        {"ContentType": "application/json"},
-    )
+    return espera_secuencia("5")
 
 
 @app.route("/api/reset", methods=["GET"])
 def api_reset():
-    ser.write(b"6")
     print("OK, Reset")
-    return json.dumps({"success": True}), 200, {"ContentType": "application/json"}
+    return espera_secuencia("6")
 
 
 @app.route("/")
 def home():
     return render_template("index.html")
+
+
+def espera_secuencia(cadena):
+    with serial.Serial("COM2", 9600) as ser:
+        ser.write(cadena.encode())
+        print("Esperando respuesta")
+        respuestas = []
+        while not ser.in_waiting:
+            continue
+        while ser.in_waiting:
+            line = ser.read_until().decode()
+            respuestas.append(
+                {
+                    "secuencia": line.split(",")[:5],
+                    "estado": line.split(",")[-1].strip(),
+                }
+            )
+            sleep(0.5)
+        res = dict((i, j) for i, j in enumerate(respuestas))
+        return (
+            json.dumps({"success": True, "response": res}),
+            200,
+            {"ContentType": "application/json"},
+        )
 
 
 if __name__ == "__main__":
